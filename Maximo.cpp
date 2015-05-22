8afb470f351e7e852538800a8a9d0de1b56cdd84
federicobruzone <federicobruzone@users.noreply.github.com>
2015-05-22 01:25:42 -0300

    Update Maximo.cpp

--- Maximo.cpp
+++ Maximo.cpp
@@ -16,13 +16,13 @@ void imprimirContenido (vector<int>);
 
 int main() {
 	
-	imprimBanner();
+	imprimBanner();  //Imprime un banner en pantalla que brinda información acerca del programa
 	cout << "Bienvenido al identificador del valor entero maximo.\n"
 	<< "Escriba un entero para agregarlo a la lista\n" 
 	<< "'M' para obtener el valor maximo\n"
 	<< "'P' para imprimir el contenido de la lista en pantalla\n" 
 	<< "'Q' para salir del programa\n";
-	recibirInput();
+	recibirInput(); //Esta funcion permite recibir nuevos valores hasta que el usario decida salir del programa
 }
 
 void imprimBanner()
@@ -40,28 +40,30 @@ void recibirInput (){
 	string input;
 	int max;
 	
-	do {
-		cin.clear();
+	do {	// El metodo de la funcion se ejecuta en un loop infinito
+		cin.clear(); // Para filtrar caracteres de retorno arrastrados de ingresos anteriores
 		cin >> input;
 		if ((input == "m") || (input == "M"))
 			cout << "El numero maximo es " << mostrarMaximo(miVector) << "\n";
-		else if ((input == "p") || (input == "P"))
+		else if ((input == "p") || (input == "P")) // La p es de 'print'
 			imprimirContenido (miVector);
 		else if ((input == "q") || (input == "Q"))
 			exit(0);
-		else
-			miVector.push_back(atoi(input.c_str()));
+		else	// Si lo ingresado no es ninguna de las letras de comando
+			miVector.push_back(atoi(input.c_str()));  // Convierte el ingreso de string a int y lo envia
+								  // al final del array dinamico miVector
 	} while (true);
 }
 
 void imprimirContenido (vector<int> mvect){
 		for (int i = 0; i < mvect.size(); i++){
-			cout << mvect[i] << " ";}
-		cout << "\n";
+			cout << mvect[i] << " "; // Imprime todos los valores en el array dinamico en una linea
+		}				 // con una separacion de un espacio
+		cout << "\n"; // Inserta un salto de linea al final
 }
 
 int mostrarMaximo(vector<int> mvect){
-	int max = mvect[0];
+	int max = mvect[0];	// Inicializa la variable con el valor de la primera posicion del array dinamico
 	
 	for (int i = 0; i < mvect.size(); i++){
 		if (mvect[i] > max)
